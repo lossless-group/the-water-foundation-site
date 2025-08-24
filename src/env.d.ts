a7deb59808d857522047522e858217bbb24c44d6
mpstaton <mpstaton@gmail.com>
2025-08-24 11:54:53 +0200

    improve, new(beliefs, pages): import pages from v-dev and create new funds as instruments pages
     On branch main
     Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/components/basics/Footer.astro
    	new file:   src/content/narratives-md/funds-as-instruments.md
    	new file:   src/env.d.ts
    	new file:   src/pages/beliefs/forprofit-non-forprofit.astro
    	renamed:    src/pages/freshwaterandoceanwater/index.astro -> src/pages/beliefs/freshwaterandoceanwater/index.astro
    	new file:   src/pages/beliefs/funds-as-instruments.astro
    	new file:   src/pages/beliefs/private-public.astro
    	renamed:    src/pages/impact/our-impact-vnt3.astro -> src/pages/impact/dashboard.astro

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="astro/client" />
+
+declare module '@layouts/*';
+declare module '@components/*';
+declare module '@sections/*';
+declare module '@infographic/*';
+declare module '@core-messages/*';
